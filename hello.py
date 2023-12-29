0220407d6eff33f1db149320a5087d5c0af33774
Davey <morales_davey@yahoo.com>
2023-12-28 19:13:28 -0600

    fetching first 386 pokemon names

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
-
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    req = requests.get(f'https://pokeapi.co/api/v2/pokemon?limit=386')
+    data = json.loads(req.content)
+    return render_template("index.html",data=data)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
